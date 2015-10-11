0d16db3fafca5b4df78952dd5f21f50a7016ff6a
Rinnegatamante <rinnegatamante@gmail.com>
2015-10-11 19:59:38 +0200

    Added songs listing and UI prototype.
    Added songs listing and UI prototype.

--- 3DS_Server/main.cpp
+++ 3DS_Server/main.cpp
@@ -2,10 +2,23 @@
 #include "Network.h"
 #include "Graphics.h"
 #include "main_font.h"
+#include "khax.h"
 #define TOP_SCREEN true
 #define BOTTOM_SCREEN false
 #define UNDER 1
 #define UPPER 0
+
+// Globals
+bool isNinjhax2;
+u32 white = 0xFFFFFFFF;
+u32 black = 0xFF000000;
+u32 red = 0xFF0000FF;
+u32 light_cyan = 0xFF48B9FF;
+u32 dark_cyan = 0xFF00B9FF;
+gpu_text* logo;
+int idx;
+Font F;
+u32 total;
 
 // Supported formats list
 enum{
@@ -22,46 +35,92 @@ typedef struct{
 } Songlist;
 
 // fetchSonglist: Gets songlist from a client
-Songlist* fetchSonglist(Socket* Client){
+Songlist* fetchSonglist(Socket* Client, u32* totalSongs){
 	socketSend(Client, "exec0:0000");
 	Packet* pkg = NULL;
 	Songlist* head = NULL;
 	Songlist* list = NULL;
-	u32 idx = 1;
+	Songlist* old = NULL;
+	u32 idx = 0;
+	*totalSongs = 0;
 	bool notReceived = true;
 	while (((pkg = socketRecv(Client, 128)) != NULL) || notReceived){	
-		if (pkg == NULL) continue;
+		if (pkg == NULL){
+			if (idx == 0) continue;
+			else{
+				old->next = NULL;
+				free(list);
+				break;
+			}
+		}
 		notReceived = false;
-		if (strncmp((char*)pkg->message, ":", 1) == 0){
+		if (pkg->size == 1){
 			free(pkg->message);
 			free(pkg);
 			return NULL;
 		}else{
-			if (idx == 1){
+			if (idx == 0){
 				list = (Songlist*)malloc(sizeof(Songlist));
 				head = list;
-				idx++;
 			}
 			char tmp[2];
 			tmp[0] = pkg->message[0];
 			tmp[1] = 0;
 			list->format = atoi(tmp);
 			strcpy(list->name, (char*)&pkg->message[1]);
-			if (pkg->message[127] == ':'){
-				list->next = (Songlist*)malloc(sizeof(Songlist));
-				list = (Songlist*)list->next;
-			}else{
-				list->next = NULL;
-				free(pkg->message);
-				free(pkg);
-				break;
-			}
+			list->next = (Songlist*)malloc(sizeof(Songlist));
+			old = list;
+			list = (Songlist*)list->next;
 			free(pkg->message);
 			free(pkg);
+			idx++;
 		}
 	}
-	socketSend(Client, "OK");
+	*totalSongs = idx;
 	return head;
+}
+
+// drawBottomUI: Draws bottom screen UI
+void drawBottomUI(){
+	initBlend(UNDER);
+	fillRect(5, 5, 310, 70, black);
+	fillRect(7, 7, 306, 66, white);
+	fillRect(5, 80, 310, 155, black);
+	fillRect(7, 82, 306, 151, white);
+	termBlend();
+}
+
+// drawTopUI: Draws top screen UI
+void drawTopUI(){
+	initBlend(UPPER);
+	drawAsset(0, 0, logo);
+	fillRect(0, 400, 220, 20, black);
+	termBlend();
+}
+
+// drawSongList: Draws SongList
+void drawSongList(Songlist* list){
+	int y = 92;
+	u32 tmp_idx = 1;
+	Songlist* pointer = list;
+	while (tmp_idx < idx){
+		pointer = (Songlist*)pointer->next;
+		tmp_idx++;
+	}
+	u32 st_idx = tmp_idx;
+	tmp_idx = 0;
+	u32 color;
+	while (tmp_idx < 8){
+		if (tmp_idx == 0) color = red;
+		else color = black;
+		F.drawString(10, y, pointer->name, Color((color >> 16) & 0xFF, (color >> 8) & 0xFF, (color) & 0xFF), BOTTOM_SCREEN, true);
+		tmp_idx++;
+		if (st_idx + tmp_idx > total) break;
+		else{
+			pointer = (Songlist*)pointer->next;
+			y = y + 17;
+		}
+	}
 }
 
 int main(){
@@ -75,58 +134,90 @@ int main(){
 	Result ret=APT_SetAppCpuTimeLimit(NULL, 30);
 	aptCloseSession();
 	CSND_initialize(NULL);
+	isNinjhax2 = false;
+	if (!hbInit()){
+		khaxInit;
+		hbExit();
+	}else isNinjhax2 = true;
+	initScene();
 	
 	// Variables definition
 	Socket* Server = NULL;
 	Socket* Client = NULL;
 	bool socketingStatus = false;
-	u32 white = 0xFFFFFFFF;
 	char IP[64];
+	char alert[64];
 	Songlist* pc_songs = NULL;
 	bool welcomeSent = false;
-	u32 oldpad;
 	
 	// Initializing resources
-	Font F;
     unsigned char* buffer = F.loadFromMemory(main_ttf,size_main_ttf);
 	F.setSize(16);
+	logo = getLogo();
+	idx = 1;
 	
 	// Main loop
 	while(aptMainLoop()){
 		hidScanInput();
-		u32 pad = hidKeysHeld();
+		u32 pad = hidKeysDown();
 		RefreshScreen();
-		ClearScreen(UPPER);
-		if (isWifiOn()){
+		drawBottomUI();
+		drawTopUI();
+		if (isWifiOn()){		
 			if (socketingStatus){
 				if (Server == NULL) Server = createServerSocket(5000);
 				else if (Client == NULL) Client = serverAccept(Server);
 				else if (!welcomeSent){
 					socketSend(Client, "WELCOME SMURF");
 					welcomeSent = true;
-				}else if (pc_songs == NULL) pc_songs = fetchSonglist(Client);
+				}else if (pc_songs == NULL){
+					pc_songs = fetchSonglist(Client, &total);
+					sprintf(alert, "Songs detected: %i", total);
+				}else{
+					drawSongList(pc_songs);				
+					F.drawString(7, 222, alert, Color((white >> 16) & 0xFF, (white >> 8) & 0xFF, (white) & 0xFF), TOP_SCREEN, true);
+					if ((pad & KEY_DUP) == KEY_DUP){
+						idx--;
+						if (idx < 1) idx = total;
+					}else if ((pad & KEY_DDOWN) == KEY_DDOWN){
+						idx++;
+						if (idx > total) idx = 1;
+					}else if ((pad & KEY_DRIGHT) == KEY_DRIGHT){
+						idx = idx + 8;
+						if (idx > total) idx = total;
+					}else if ((pad & KEY_DLEFT) == KEY_DLEFT){
+						idx = idx - 8;
+						if (idx < 1) idx = 1;
+					}
+				}
 			}else{
 				initSocketing();
 				socketingStatus = true;
 			}
 		}else{
-		
 		}
 		getIPAddress(IP);
-		F.drawString(10, 50, IP, Color((white >> 16) & 0xFF, (white >> 8) & 0xFF, (white) & 0xFF), TOP_SCREEN, true);
+		char ip_addr[32];
+		sprintf(ip_addr, "IP: %s", IP);
+		F.drawString(257, 222, ip_addr, Color((white >> 16) & 0xFF, (white >> 8) & 0xFF, (white) & 0xFF), TOP_SCREEN, true);
 		gfxFlushBuffers();
 		gfxSwapBuffers();
 		gspWaitForVBlank();
 		if ((pad & KEY_START) == KEY_START){
-			socketSend(Client, "exec3:0000");
-			socketClose(Client);
-			socketClose(Server);
+			free(logo->tex);
+			free(logo);
+			if (Client != NULL){
+				socketSend(Client, "exec3:0000");
+				socketClose(Client);
+			}
+			if (Server != NULL) socketClose(Server);
 			break;
 		}
-		oldpad = pad;
 	}
 	
 	// Term services
+	if (!isNinjhax2) khaxExit();
+	endScene();
 	CSND_shutdown();
 	if (socketingStatus) termSocketing();
 	hidExit();
